a5b133298d3cfe6f1404b2cbca6affb4c2f4487e
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2022-05-20 04:34:15 +0900

    0520

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,8 +1,126 @@
-function Home(){
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { getMovies, IGetMoivesResult } from '../api';
+import { makeImagePath } from '../utils';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react'
 
+
+const Wrapper = styled.div`
+    background-color: black;
+`;
+
+const Loader = styled.div`
+    height: 20vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Banner = styled.div<{bgPhoto: string}>`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 60px;
+    background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)) , url(${(props => props.bgPhoto)});
+    background-size: cover;
+`;
+
+const Title = styled.h2`
+    font-size: 55px;
+    margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+    font-size: 25px;
+    width: 50%;
+`;
+
+const Slider = styled.div`
+    top: -100px;
+    position: relative;
+`;
+
+const Row = styled(motion.div)`
+    display: grid;
+    gap: 5px;
+    grid-template-columns: repeat(6, 1fr);
+    position: absolute;
+    width: 100%;
+`;
+
+const Box = styled(motion.div)<{bgPhoto: string}>`
+    background-color: #fff;
+    height: 200px;
+    background-image: url(${props => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.innerWidth + 5,
+    },
+    visible: {
+        x: 0,
+    },
+    exit: {
+        x: -window.innerWidth - 5,
+    }
+};
+
+const offset = 6;
+
+function Home(){
+    // ueseQuery Hook : fetch를 사용해서 data와 isLoading(아직 로딩중인지)에 대한 알림을 전해줌
+    // <IGetMoivesResult> : 가져온 데이터의 타입을 명시
+    const {data, isLoading } = useQuery<IGetMoivesResult>(["movies, nowPlaying"], getMovies);
+    // console.log(data, isLoading);
+    // console.log(data?.results[0].backdrop_path);
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if(data){
+            if(leaving) return;
+            toggleLeaving();
+            const totalMovies = data?.results.length -1;
+            const maxIndex = Math.floor(totalMovies / offset) -1;
+            setIndex((prev) => prev=== maxIndex ? 0 : prev+1 );
+        }
+    };
+    const toggleLeaving = () => setLeaving(prev => !prev);
     return(
-        <div style={{backgroundColor:"white", height:"200vh"}}>
-        </div>
+        <Wrapper>
+            {isLoading ? 
+            <Loader>Loading</Loader> :
+            <>
+                <Banner onClick={increaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                    <Title>{data?.results[0].title}</Title>
+                    <Overview>{data?.results[0].overview}</Overview>
+                </Banner>
+
+                <Slider>
+                    {/* onExitComplete: exit이 끝났을 때 실행 */}
+                    <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                        <Row variants={rowVariants} 
+                             initial="hidden" 
+                             animate="visible"
+                             exit="exit"
+                             transition={{type:"tween", duration: 1}}
+                             key={index}>
+                          {data?.results.slice(1).slice(offset*index, offset*index + offset)
+                          .map(movie => <Box 
+                                            key={movie.id}
+                                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")} 
+                                        />)}
+                        </Row>
+                    </AnimatePresence>
+                </Slider>
+            </>}
+        </Wrapper>
     );
 }
 
--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,33 @@
-// const API_KEY = "08eae69feffb85e384809388866cf0a2";
+const API_KEY = "08eae69feffb85e384809388866cf0a2";
+const BASE_PATH = 'https://api.themoviedb.org/3/';
+// https://api.themoviedb.org/3/movie/now_playing?api_key=08eae69feffb85e384809388866cf0a2&language=en-US&page=1
+// https://image.tmdb.org/t/p/original/
+
+
+// typescript 사용하기 위해서는 API 응답의 타입을 지정해 주어야 함 => 인터페이스를 지정 => 자동완성 사용가능
+// 영화 데이터 내의 results 배열에 대한 I(interface)
+// 필요한 부분만 적는다
+export interface IMoive{
+  id: number;            // id
+  backdrop_path: string; // 배경 포스터
+  poster_path: string;   // 포스터
+  title: string;         // 제목
+  overview: string;      // 요약 줄거리
+}
+
+// 영화 데이터 결과에 대한 I(interface)
+export interface IGetMoivesResult {
+  dates: {
+    maximum: string;
+    minimum: string; 
+  };
+  page: number;
+  results: IMoive[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies(){
+  // fetch : 데이터를 받아오고 JSON을 리턴하는 역할(함수)
+  return fetch(`${BASE_PATH}movie/now_playing?api_key=${API_KEY}`).then(res => res.json());
+}
